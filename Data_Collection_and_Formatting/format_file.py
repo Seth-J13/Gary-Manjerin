7e6604398762ad5f311c2621bce29bf39a7413c8
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-05-23 17:35:57 -0500

    Update format_file.py

--- Data_Collection_and_Formatting/format_file.py
+++ Data_Collection_and_Formatting/format_file.py
@@ -2,12 +2,69 @@ import shapefile
 
 # Change this path when you're testing
 # If we need to input files easier, we can ask user to input a path instead of hard-coding it
-default_path = "C:/Users/xrock/OC_Classes/AI_Project_T1/tx_2020_gen_2020_blocks/"
 
-sf = shapefile.Reader(default_path + "tx_2020_gen_2020_blocks.shp")
+#Seth's path
+#default_path = "C:/Users/xrock/OC_Classes/AI_Project_T1/tx_2020_gen_2020_blocks/"
+
+#Ethan's file path
+default_path = "C:/College/SummerClasses/"
+
+#Seth's shape file
+#sf = shapefile.Reader(default_path + "tx_2020_gen_2020_blocks.shp")
+
+
+
 # rec = sf.records()[0:10]
 # fields = sf.fields
 # print([x.name for x in sf.fields[1:]])
-firstRec = sf.record(1)
-for i in range(len(firstRec)):
-    print(str(sf.fields[i]) +  " " + str(firstRec[i]) + "\n")
+#firstRec = sf.record(1)
+#for i in range(len(firstRec)):
+#    print(str(sf.fields[i]) +  " " + str(firstRec[i]) + "\n")
+    
+    
+#Ethan's code. It may or may not work, so just comment it out when you are testing your code
+#Ethan's shape file reader
+sf = shapefile.Reader("oktraining.shp")
+
+#get records and shapes
+records = sf.records()
+shapes = sf.shapes()
+
+for i in range(len(records)):
+
+    record = records[i]
+    shape = shapes[i]
+    
+    #separate records into variables
+    #on assignment sheet: "The fields in the file are: ID, Longitude, Latitude, Population, 
+        #Total Votes, Republican Vote Share, and Democratic Vote Share, in that order"
+    lineId = record[0]
+    population = record[1]
+    presidentVotes = record[2]
+    senateVotes = record[3]
+    congressVotes = record[4]
+    republicanVotes = record[5]
+    democratVotes = record[6]
+
+    #get bounding box (top left corner, bottom right corner)
+    bbox = shape.bbox
+
+    xmin = bbox[0]
+    ymin = bbox[1]
+    xmax = bbox[2]
+    ymax = bbox[3]
+
+    # Get center of bounding box
+    longitude = (xmin + xmax) / 2
+    latitude = (ymin + ymax) / 2
+
+    #calculate votes
+    total_votes = pres_votes + senate_votes + congress_votes
+
+    #check if total votes aren't zero to prevent dividing by zero
+    if total_votes > 0:
+        rep_share = rep_votes / total_votes
+        dem_share = dem_votes / total_votes
+    else:
+        rep_share = 0
+        dem_share = 0
