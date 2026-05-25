8d6cf67dccfdf3ff1813eb28a1137509ded20f89
Seth-J13 <seth.jack@eagles.oc.edu>
2026-05-25 13:13:28 -0500

    CSV

--- Data_Collection_and_Formatting/format_file.py
+++ Data_Collection_and_Formatting/format_file.py
@@ -1,16 +1,18 @@
 import shapefile
+import numpy as math
+import pathlib as path
 
 # Change this path when you're testing
 # If we need to input files easier, we can ask user to input a path instead of hard-coding it
 
 #Seth's path
-#default_path = "C:/Users/xrock/OC_Classes/AI_Project_T1/tx_2020_gen_2020_blocks/"
+default_path = "C:/Users/xrock/OC_Classes/AI_Project_T1/tx_2020_gen_2020_blocks/"
 
 #Ethan's file path
-default_path = "C:/College/SummerClasses/"
+# default_path = "C:/College/SummerClasses/"
 
 #Seth's shape file
-#sf = shapefile.Reader(default_path + "tx_2020_gen_2020_blocks.shp")
+sf = shapefile.Reader(default_path + "tx_2020_gen_2020_blocks.shp")
 
 
 
@@ -24,11 +26,14 @@ default_path = "C:/College/SummerClasses/"
     
 #Ethan's code. It may or may not work, so just comment it out when you are testing your code
 #Ethan's shape file reader
-sf = shapefile.Reader("oktraining.shp")
+# sf = shapefile.Reader("oktraining.shp")
 
 #get records and shapes
 records = sf.records()
 shapes = sf.shapes()
+
+#list for use in csv
+training_data = []
 
 for i in range(len(records)):
 
@@ -46,6 +51,8 @@ for i in range(len(records)):
     republicanVotes = record[5]
     democratVotes = record[6]
 
+    # print("President Votes: " + str(presidentVotes) + " Senate Votes: " + str(senateVotes) + " Congress Votes: " + str(congressVotes))
+
     #get bounding box (top left corner, bottom right corner)
     bbox = shape.bbox
 
@@ -59,12 +66,19 @@ for i in range(len(records)):
     latitude = (ymin + ymax) / 2
 
     #calculate votes
-    total_votes = pres_votes + senate_votes + congress_votes
+    try:
+        total_votes = float(presidentVotes) + float(senateVotes) + float(congressVotes)
+    except (ValueError, TypeError):
+        total_votes = 0
 
     #check if total votes aren't zero to prevent dividing by zero
     if total_votes > 0:
-        rep_share = rep_votes / total_votes
-        dem_share = dem_votes / total_votes
+        rep_share = republicanVotes / total_votes
+        dem_share = 1 - rep_share
     else:
         rep_share = 0
         dem_share = 0
+
+    assembled_data = [population, (longitude, latitude), rep_share, dem_share]
+    training_data.append(assembled_data)
+path.Path("training_data.csv").write_text(str(training_data))
