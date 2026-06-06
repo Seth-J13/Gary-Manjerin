b123a395385ca4ec1fdad0c433e1532ad515edac
Seth-J13 <seth.jack@eagles.oc.edu>
2026-06-06 10:55:27 -0500

    tried to fix file stuff

--- Predictive_Model/predict.py
+++ Predictive_Model/predict.py
@@ -6,6 +6,15 @@ import numpy as np
 import os.path as osPath
 from pathlib import Path as path
 
+
+###
+# The folder finder does not work. Alex is better at the os path stuff. If you(Alex) want to work on it, you can. 
+# Otherwise, we can just keep findFiles() func to get the folders.
+###
+
+
+
+
 training_path = ""
 testing_path = ""
 
@@ -23,32 +32,32 @@ def findFiles():
     return training_path, testing_path
 
 # This searches the entire computer for shape files.
-def search_computer_for_shape_files():
+def search_computer_for_training_and_testing_dirs():
     print("Searching... (this may take a while)\n")
     write_list = ""
-    shapefile_list = []
-    for _path in path.Path(osPath.expanduser("~")).rglob("*.shp"):
+    folder_list = []
+    for _path in path(osPath.expanduser("~")).rglob("*"):
         write_list = write_list + str(_path) + ","
-        shapefile_list.append(str(_path))
+        folder_list.append(str(_path))
 
     # Build a database so the search doesn't need to be conducted again.
-    with open("shapefiles.csv", "w") as shapefiles:
-        shapefiles.write(write_list[:-1])
-    shapefiles.close()
+    with open("folder_list.csv", "w") as folder_lists:
+        folder_lists.write(write_list[:-1])
+    folder_lists.close()
 
-    for _item in shapefile_list:
-        shapefile_list[shapefile_list.index(_item)] = _item.replace("\\", "/")
+    for _item in folder_list:
+        folder_list[folder_list.index(_item)] = _item.replace("\\", "/")
 
-    return shapefile_list
+    return folder_list
 
 # This checks the stored database of shape files in the same directory.
 # Used to save time while testing and executing
 def check_database():
-    with open("shapefiles.csv", "r") as shapefiles:
-        shapefile_list = shapefiles.read().replace("\\", "/").strip().split(",")
+    with open("folder_list.csv", "r") as folder_lists:
+        folder_lists = folder_lists.read().replace("\\", "/").strip().split(",")
         
-    shapefiles.close()
-    return shapefile_list
+    folder_lists.close()
+    return folder_lists
 
 
 training_path, testing_path = findFiles()
@@ -62,25 +71,29 @@ while True:
         break
 
 if choice == "1":
-    if (osPath.exists("shapefiles.csv")):
+    if (osPath.exists("folder_list.csv")):
         if (input("\nYour previous search was saved to a database! You can use that instead of checking again.\nSearch anyway? (y/n) ") == "y"):
-            shapefile_list = search_computer_for_shape_files()
+            folder_list = search_computer_for_training_and_testing_dirs()
         else:
-            shapefile_list = check_database()
+            folder_list = check_database()
     else:
-        shapefile_list = search_computer_for_shape_files()
+        folder_list = search_computer_for_training_and_testing_dirs()
 
 # Let the user opt to upload a file using a file-picker dialog
 elif choice == "2":
     selected_path = tkinter.filedialog.askopenfilename()
 
 # If the user neglects the whole search, instead use a database stored on the computer
-elif choice == "3" and (os.path.exists("shapefiles.csv")):
-    shapefile_list = check_database()
+elif choice == "3" and (osPath.exists("folder_list.csv")):
+    folder_list = check_database()
 
 # Choose from list of shape files that were cached or detected
 if choice == "1" or choice == "3":    
-    common_path = osPath.commonprefix(shapefile_list)
-    for x in range(len(shapefile_list)):
-        print(str(x) + ": " + str(shapefile_list[x])[str(shapefile_list[x]).rfind("/") + 1:]) 
-    selected_path = shapefile_list[int(input("Please enter the index of the file you would like to use (start at 0 and count up): "))]
+    common_path = osPath.commonprefix(folder_list)
+    for x in range(len(folder_list)):
+        print(str(x) + ": " + str(folder_list[x])[str(folder_list[x]).rfind("/") + 1:]) 
+    selected_path = folder_list[int(input("Please enter the index of the file you would like to use (start at 0 and count up): "))]
+
+
+
+
