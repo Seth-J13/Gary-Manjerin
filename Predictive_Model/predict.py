6b58774a4b0051f3f05b97b0b62d47d4d5f2cc88
alexander-stockton <alexander.stockton@eagles.oc.edu>
2026-06-11 23:45:16 -0500

    cleaned up code, made many things simpler, optimized a little bit, explained with many comments

--- Predictive_Model/predict.py
+++ Predictive_Model/predict.py
@@ -1,11 +1,4 @@
-# import tensorflow as tf
 import tkinter
-
-# #from claude AI VVVV
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.model_selection import train_test_split
-# # ^^^^
 
 import csv
 import math
@@ -14,14 +7,18 @@ import os.path as osPath
 import sklearn as sklearn
 from pathlib import Path as path
 
-
-# I saw your note. This worked with a few minor tweaks
+# ----------------------------------------------------------------------- #
+# - findFiles() detects the training and testing folders on your system - #
+# - this requires you to have run format_file.py first                  - #
+# ----------------------------------------------------------------------- #
 training_path = ""
 testing_path = ""
 
 def findFiles():
-    working_dir = path.cwd()
-    working_dir = working_dir.parent.as_posix() + "/Data_Collection_and_Formatting"
+    working_dir = path.cwd() # Get current file path
+    working_dir = working_dir.parent.as_posix() + "/Data_Collection_and_Formatting" # Modify for navigation
+    
+    # If the previous folder exists on the computer already, add the training and testing folders
     if(osPath.exists(working_dir)):
         training_path = working_dir + "/training"
     else:
@@ -31,14 +28,9 @@ def findFiles():
     else:
         print("No testing path exists")
     return training_path, testing_path
-training_path, testing_path = findFiles()
+training_path, testing_path = findFiles() # Grab the training and testing folders
 
-# Rounds data out to be proportionately between 0 and 1 with floating point accuracy
-def normalize(value, min_value, max_value):
-    normalized_value = ((math.log(1.0 + value) - math.log(1.0 + min_value)) / (math.log(1.0 + max_value) - math.log(1.0 + min_value)))
-    return normalized_value
-
-# Not sure what you did here, but it works
+# Use the detected file paths from above to grab the training and testing CSV files for our model
 print("\nSelect a file to use for training the model.\n")
 paths = path.rglob(path(training_path), "*.csv")
 paths_list = tuple(paths)
@@ -47,23 +39,22 @@ for x in paths_list:
     print(str(num) + ": " + str(x))
     num += 1
 
-num = int(input("Please enter the index of the file you would like to use (start at 0 and count up): "))
+# Let the use select which training and testing file to use
+num = -1
+while num < 0 or num > paths_list.__len__():
+    num = int(input("Please enter the index of the file you would like to use (start at 0 and count up): "))
 selected_training_path = paths_list[num]
 
-paths = path.rglob(path(testing_path), "*.csv")
+paths = path.rglob(path(testing_path), "*.csv") # <-- Automatically select the testing file based on training
 paths_list = tuple(paths)
 selected_testing_path = paths_list[num]
 
 # Initialize input and output arrays
 X, y = [],[]
-correct_answers = []
+correct_answers = [] # <-- this just shows the real vote shares during training
 
-max_y, max_x = 0.0, 0.0     # These need to start low so everything is higher than them
-min_y, min_x = 999.0, 999.0 # These need to start high so everything is lower than them
-
-# We need to keep track of these so we know which lines we use later for testing
-row_number = 0
-rows_visited = []
+max_y, max_x = 0.0, 0.0         # These need to start low so everything is higher than them
+min_y, min_x = 99999.0, 99999.0 # These need to start high so everything is lower than them
 
 # --------------
 # Begin training
@@ -71,22 +62,26 @@ rows_visited = []
 with open(selected_training_path) as train:
     filereader = csv.reader(train)
     
-    row_n = 0
+    # Collect all the proper vote shares already existing in the data (includes zero'd entries)
     for row in filereader:
-        rows_visited.append(row_number)
-        X.append([float(row[0]), float(row[1]), float(row[2]), float(row[3])]) # Given the first 3 columns
+        total_share = float(row[4]) + float(row[5]) # Total up vote share
+        
+        # Normalize the vote shares if the sum of them is over 100%, otherwise add them raw
+        if total_share >= 1:
+            r_percent = float(row[4]) / float(total_share) # Normalize republican share
+            d_percent = float(row[5]) / float(total_share) # Normalize democrat share
+            correct_answers.append([r_percent, d_percent])
+        else:
+            correct_answers.append([float(row[4]), float(row[5])])
 
+    # Restart the file reader so we can do the actual training
+    train.seek(0)
+    filereader = csv.reader(train)
+    for row in filereader:
         # Calculate normalized percentage of republican and democrat votes returned
         if float(row[3]) != 0.0 and int(row[2]) != 0: # Skip and auto-set to 0 if there is no population in this block
-            total_share = float(row[4]) + float(row[5]) # Total up vote share
-            r_percent = float(row[4]) / float(total_share) # Calculate republican share
-            d_percent = float(row[5]) / float(total_share) # Calculate democrat share
-            y.append(d_percent) # Predict the 6th column (5th column is just [1.0 - 4th_column])
-        else:
-            r_percent = 0
-            d_percent = 0
-            y.append(d_percent)
-        correct_answers.append([float(r_percent), float(d_percent)])
+            X.append([float(row[0]), float(row[1]), float(row[2]), float(row[3])]) # Given the first 3 columns
+            y.append(correct_answers[filereader.line_num-1][1]) # Predict the normalized 6th column (normalized 5th column is just [1.0 - 4th_column])
 
         # Storing min and max X and Y values this way greatly increases performance rather
         # than using built in functions to find the min and max of the whole array after it's built
@@ -100,85 +95,95 @@ with open(selected_training_path) as train:
         if float(row[2]) < min_x:
             min_x = float(row[2]) if float(row[2]) != 0 else min_x
 
-        # # This skips lines randomly (but never 5 times in a row)
-        # jumper = np.random.randint(0,4)
-        # for _ in range(jumper):
-        #     try:
-        #         filereader.__next__()
-        #     except StopIteration:
-        #         break
-        # row_number += jumper + 1 # Include random jump and this line to row number
+        # This skips lines randomly (but never 5 times in a row)
+        jumper = np.random.randint(0,4)
+        for _ in range(jumper):
+            try:
+                filereader.__next__()
+            except StopIteration:
+                continue
 
     # Normalize our Population and Predicted Democrat Values for statistical stability
-    normalized_x = []
-    for _ in X:
-        if _[2] == 0: # Ignore if 0 population
-            break
-        else:
-            _[2] = math.log(1+_[2]) # Normalize population
-        if _[3] == 0: # Ignore if 0 votes
-            break
-        else:
-            _[3] = math.log(1+_[3]) # Normalize vote count
-        
-    
-    # # This loop scheme greatly increases perfomance at the cost of being ugly
-    # line = 0
     # for _ in X:
-    #     _[2] = normalized_x[line]
-    #     line += 1
+    #     if _[2] == 0: # Ignore if 0 population
+    #         break
+    #     else:
+    #         _[2] = math.log(1+_[2]) # Normalize population
+    #     if _[3] == 0: # Ignore if 0 votes
+    #         break
+    #     else:
+    #         _[3] = math.log(1+_[3]) # Normalize vote count
 
-# Similar casing added for redundancy and east of use for people with different typing styles.
-# If we find this unnecessary, we can remove it later
-training_kernel = ""
-while training_kernel.lower() not in ["linear", "rbf", "poly", "sigmoid"]:
-    training_kernel = input("\nSelect a kernel for the SVM model\n- linear (somewhat fast but less accurate)\n- rbf (terribly slow, generally more accuracy)\n- poly (fairly quick, much less accurate) \n- sigmoid (no)\n")
+# List to use the same selection scheme as before, but for kernels
+kernels = ["linear", "rbf"]
+kernel_descriptions = [" (fast but less accurate)", " (slow but more accurate)"]
+print("\nSelect a kernel (learning style) to train the predictive model on:\n")
+for _ in kernels:
+    print(str(kernels.index(_)) + ". " + _ + kernel_descriptions[kernels.index(_)]) 
 
+training_kernel = -1 # Keep asking for input until it's within range
+while training_kernel < 0 or training_kernel > kernels.__len__():
+    training_kernel = int(input("\nSelect a kernel for the SVM model (start from index 0 and count up): "))
+training_kernel = kernels[training_kernel]
+
+# Inform the user we're training the model
 print("\nTraining model... This might take a really long time!\n")
-print("Note: results might look the same twice with only a few small differences.\n")
 
 # Define the model and its characteristics
 prediction_model = None
-if training_kernel.lower() == 'linear':
+if training_kernel == 'linear':
     prediction_model = sklearn.pipeline.Pipeline([
         ('scaler', sklearn.preprocessing.StandardScaler()), # This scales everything to within significantly narrow range
-        ('svm',    sklearn.svm.LinearSVR(loss='squared_epsilon_insensitive', C=1000, epsilon=0.1))
-    ])
-elif training_kernel.lower() == 'poly':
-    prediction_model = sklearn.pipeline.Pipeline([
-        ('scaler', sklearn.preprocessing.StandardScaler()), # This scales everything to within significantly narrow range
-        ('poly_approx', sklearn.kernel_approximation.PolynomialCountSketch(degree=8, n_components=300, gamma=0.2)),
-        ('sgd', sklearn.linear_model.SGDRegressor())
+        ('svm',    sklearn.svm.LinearSVR(loss='squared_epsilon_insensitive', C=1.5, epsilon=0.1))
     ])
 else:
     prediction_model = sklearn.pipeline.Pipeline([
         ('scaler', sklearn.preprocessing.StandardScaler()), # This scales everything to within significantly narrow range
-        ('svm',    sklearn.svm.SVR(kernel=training_kernel.lower(), C=1.5, gamma='scale'))
+        ('svm',    sklearn.svm.SVR(kernel=training_kernel.lower(), C=10, gamma=0.1))
     ])
 
 prediction_model.fit(X, y) # Train model
 
 print("Model Trained!\nTesting its accuracy now...\n")
 
+# -------------
+# Begin Testing
+# -------------
 with open(selected_testing_path) as test:
     filereader = csv.reader(test)
-    line = 0
+    
     correct_index = 0 # This tracks the index of the correct_answers array instead of the line number
 
+    # Initialize output variables
     democrats = 0.0
     republicans = 0.0
+    deviations = []
+
+    # Test through every line in the CSV
     for row in filereader:
-        # if line in rows_visited:
         if float(row[2]) == 0.0: # This is used to breeze past areas with 0 population
             republicans = 0.0
             democrats = 0.0
         else:
-            democrats = prediction_model.predict([[float(row[0]), float(row[1]), float(row[2]), float(row[3])]])
-            republicans = 1.0 - democrats
-        if type(democrats) == np.ndarray:
-            print(f"Predicted Vote Shares: {republicans[0]*100:.2f}% Republican | {democrats[0]*100:.2f}% Democrat | Actual Vote Shares: {correct_answers[correct_index][0]*100:.2f}% Republican | {correct_answers[correct_index][1]*100:.2f}% Democrat")
-        else:
-            print(f"Predicted Vote Shares: {republicans*100:.2f}% Republican | {democrats*100:.2f}% Democrat | Actual Vote Shares: {correct_answers[correct_index][0]*100:.2f}% Republican | {correct_answers[correct_index][1]*100:.2f}% Democrat")
-        correct_index += 1
-        line += 1
+            democrats = prediction_model.predict([[float(row[0]), float(row[1]), float(row[2]), float(row[3])]])[0] # Predict vote share
 
+            # Clamp democrat results to between 0.01% and 99.99%
+            democrats = 0.9999 if democrats > 0.9999 else democrats
+            democrats = 0.0001 if democrats < 0.0001 else democrats
+
+            # Calculate republican votes based on democrats (ignore other parties since that would require four separate Support-Vector-Regressors)
+            republicans = 1.0 - democrats
+
+            # Calculate deviation from correct scores by updating current deviation with the average between the current and previous deviation
+            deviations.append(abs(correct_answers[correct_index][1] - democrats)/2)
+
+            print(f"Predicted Vote Shares: {republicans*100:.2f}% Republican | {democrats*100:.2f}% Democrat\t\tActual Vote Shares: {correct_answers[correct_index][0]*100:.2f}% Republican | {correct_answers[correct_index][1]*100:.2f}% Democrat\nCurrent Deviation: {abs(correct_answers[correct_index][1] - democrats)*100:.2f}%\n")
+        
+        correct_index += 1 # This updates what index of the correct_answers list we're on
+
+    # Print results
+    total_deviation = 0
+    for d in deviations:
+        total_deviation = total_deviation + d
+    total_deviation = total_deviation/deviations.__len__()
+    print(f"Final Accuracy: {(1-total_deviation)*100:.2f}%")
