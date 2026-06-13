f3f7da889d3021ff8adf46c2933bb9d018cd39da
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-06-13 13:12:44 -0500

    Started Part 3 of our Gerrymandering project

---
+++ Reasonable_Gerrymandering/Gerrymander.py
@@ -0,0 +1,17 @@
+# import tensorflow as tf
+import tkinter
+
+# #from claude AI VVVV
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import train_test_split
+# # ^^^^
+
+import csv
+import math
+import numpy as np
+import os.path as osPath
+import sklearn as sklearn
+from pathlib import Path as path
+
+
