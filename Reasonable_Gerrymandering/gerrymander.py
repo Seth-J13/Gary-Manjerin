88012d9c0768191d0d4f60dccd0c0bc03ab1f923
Seth-J13 <seth.jack@eagles.oc.edu>
2026-06-15 13:26:27 -0500

    list the result files

--- Reasonable_Gerrymandering/gerrymander.py
+++ Reasonable_Gerrymandering/gerrymander.py
@@ -1,10 +1,21 @@
 
 import os.path as osPath
-import pathlib as path
+from pathlib import Path as path
 
 def GetFiles():
+    working_dir = path.cwd() # Get current file path
+    working_dir = working_dir.parent.as_posix() + "/Predictive_Model/prediction/" # Modify for navigation
     
-    return
+    # If the previous folder exists on the computer already, add the training and testing folders
+    if(osPath.exists(working_dir)):
+        return path.rglob(path(working_dir), "*.csv")
+    else:
+        return print("No prediction path exists")
 
 
-result_file = GetFiles()
+result_files = GetFiles()
+result_files = tuple(result_files)
+num = 0
+for x in result_files:
+    print(str(num) + ": " + str(x)[-35:])
+    num += 1
