7badb9da70d3ee0d41ff148e966568635cd1a352
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-06-14 19:54:33 -0500

    Update Gerrymander.py

--- Reasonable_Gerrymandering/Gerrymander.py
+++ Reasonable_Gerrymandering/Gerrymander.py
@@ -15,3 +15,20 @@ import sklearn as sklearn
 from pathlib import Path as path
 
 
+training_path = ""
+testing_path = ""
+
+#copying the file finding code from part 2
+def findFiles():
+    working_dir = path.cwd()
+    working_dir = working_dir.parent.as_posix() + "/Data_Collection_and_Formatting"
+    if(osPath.exists(working_dir)):
+        training_path = working_dir + "/training"
+    else:
+        print("No training path exists")
+    if(osPath.exists(working_dir)):
+        testing_path = working_dir + "/testing"
+    else:
+        print("No testing path exists")
+    return training_path, testing_path
+training_path, testing_path = findFiles()
