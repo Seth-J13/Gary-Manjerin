bedb96bb2756ceeb8538d72ae83e8fa25ed7a873
Seth-J13 <seth.jack@eagles.oc.edu>
2026-06-16 14:47:52 -0500

    broke but progress

--- Reasonable_Gerrymandering/Gerrymander.py
+++ Reasonable_Gerrymandering/Gerrymander.py
@@ -1,7 +1,7 @@
 import os.path as osPath
 from pathlib import Path as path
-
-
+import bisect as split
+import numpy as math
 ###################################################################################################
 # Get Files is the starting function that retrieves the csv result files from the previous part
 # It returns a iterable tuple for the program to use
@@ -52,6 +52,13 @@ def find_lowest(high_pops):
             min = tuple(pop)
     return min, list(high_pops).index(min)
 ###################################################################################################
+def Index(id):
+    # print(id)
+    for block in list_of_blocks:
+        # print(block)
+        if id == block[0]:
+            return block
+###################################################################################################
 
 #Start of program, get the files and ask which files to use
 result_files = GetFiles()
@@ -70,8 +77,6 @@ with open(result_files[choice], "r") as state:
     # the formatting of each block VVV
     # id[0], longtitude[1], latitude[2], population[3], democrat share[4], republican share[5]
     list_of_blocks = []
-    sorted_by_lat = []
-    sorted_by_lon = []
     highest_pops = []
     # Separates the blocks and their values into a 2D array(list) and finds number of democrat districts of higest district populations
     for block in state:
@@ -87,9 +92,8 @@ with open(result_files[choice], "r") as state:
         dem_share = float(block[5])
 
         # finding the higest N populations
-        list_of_blocks.append([id, lon, lat, population, rep_share, dem_share])
-        sorted_by_lat.append(tuple([id, lat]))
-        sorted_by_lon.append(tuple([id, lon]))
+        list_of_blocks.append(tuple([id, lon, lat, population, rep_share, dem_share]))
+
         if len(highest_pops) < democrats:
             highest_pops.append(tuple([id, population]))
         else:
@@ -97,12 +101,31 @@ with open(result_files[choice], "r") as state:
             if(population > lowest[1]):
                 highest_pops[index] = tuple([id, population])
     # print(sorted_by_lat)
-    sorted_by_lat.sort(key=lambda block: block[1])
-    sorted_by_lon.sort(key=lambda block: block[1])
+    list_of_blocks.sort(key=lambda block: block[2])
+    highest_pops.sort()
     state.close()
 
 # Using the highest populations we begin to seed the districts onto the map
+neighbors = {}
+minDist = 0.001
+lat_values = [block[2] for block in list_of_blocks]
+num = 0
+for block in list_of_blocks:
+    left_idx = split.bisect_left(lat_values, block[2] - minDist)
+    right_idx = split.bisect_right(lat_values, block[2] + minDist)
+    print("lat: " + str(block[2]) + "| L: " + str(left_idx) + " | R: " + str(right_idx))
+    nearestNeighbors = {}
+    distRange = right_idx - left_idx
+    for x in range(distRange):
+        if(x + left_idx != Index(block[0])[0]):
+            nearestNeighbors.update({str(x) : str(list_of_blocks[left_idx + x][0])})
+    neighbors.update({block[0] : nearestNeighbors})
+    num += 0
+    print(neighbors)
+    # minDist = math.sqrt(math.square())
 
-                
+
+
+
 
 
